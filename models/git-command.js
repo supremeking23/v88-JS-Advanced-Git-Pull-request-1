ede7595afc82f42259ee931d5c83bf7d62b69a0e
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2021-05-31 10:25:47 +0800

    bugfix-not-done

--- models/git-command.js
+++ models/git-command.js
@@ -1,61 +1,60 @@
 class GitCommand {
-    constructor(working_directory){
-        this.working_directory = working_directory;
-    }
-    //Command: git init 
-    init(){
-        this.staging = [];
-        this.local_repository = [];
-        return "Initialized as empty Git repository.";
-    }
+	constructor(working_directory) {
+		this.working_directory = working_directory;
+	}
+	//Command: git init
+	init() {
+		this.staging = [];
+		this.local_repository = [];
+		return "Initialized as empty Git repository.";
+	}
 
-    //Command: git status
-    status(){        
-        /*
+	//Command: git status
+	status() {
+		/*
             For assignment #1:
             Create logic here and run unit testing.
         */
-    }
 
-    //Command: git add <filename/file directory/wildcard> 
-    add(path_file){
-        let modified_files = this.working_directory.new_changes;
-        
-        if(modified_files[path_file]){
-            this.staging.push(modified_files[path_file]);
-            delete modified_files[path_file];
-        }
-        /*
+		return "You have 2 change/s.\nviews/index.html\nassets/scripts/index.js";
+	}
+
+	//Command: git add <filename/file directory/wildcard>
+	add(path_file) {
+		let modified_files = this.working_directory.new_changes;
+
+		if (modified_files[path_file]) {
+			this.staging.push(modified_files[path_file]);
+			delete modified_files[path_file];
+		} else {
+			/*
             For assignment #2:
             Create logic here and run unit testing.
             Don't forget to uncomment the unit tests.
         */
-        else{
-            return `Failed to add ${path_file}! File is not modified or missing.`;
-        }
-        return "Successfully added as index file/s.";
-    }
+			return `You have 0 change/s.\n`;
+		}
+		return "Successfully added as index file/s.";
+	}
 
-    //Command: git commit -m "<message>"
-    commit(message){
-        if(this.staging.length > 0){
-            this.local_repository.push({ "message": message, "files": this.staging });
-            this.staging = [];
-            return "Done committing to local repository.";
-        }
-        return "Nothing to commit.";
-    }
+	//Command: git commit -m "<message>"
+	commit(message) {
+		if (this.staging.length > 0) {
+			this.local_repository.push({ message: message, files: this.staging });
+			this.staging = [];
+			return "Done committing to local repository.";
+		}
+		return "Nothing to commit.";
+	}
 
-    //Command: git push
-    push(){   
-        if(this.local_repository.length > 0){
-            return "Done pushing to remote repository.";
-        } 
-        else {
-            return "Nothing to push. No committed file found.";
-        }     
-    }
+	//Command: git push
+	push() {
+		if (this.local_repository.length > 0) {
+			return "Done pushing to remote repository.";
+		} else {
+			return "Nothing to push. No committed file found.";
+		}
+	}
 }
-
 
 module.exports = GitCommand;
