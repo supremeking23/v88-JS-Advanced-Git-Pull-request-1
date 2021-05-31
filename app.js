de268c00de6e2a7ed6baf56d5fcf512a0bb97444
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2021-05-31 10:42:52 +0800

    testing

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ wd.addFile("index.js", "assets/scripts", "alert('Hi!')");
 
 let git = new GitCommand(wd);
 
-
 //1. Initialize git workspace
 git.init();
 
@@ -26,3 +25,5 @@ console.log(git.commit("Added index.html file"));
 
 //6. Push
 console.log(git.push());
+
+// tesrt
